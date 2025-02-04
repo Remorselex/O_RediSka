eba22780ee059df4847447896d6956fe08494f65
Aleh Astrouski <aleh.astrouski@softnetix.io>
2025-02-04 23:00:12 +0300

    init

---
+++ BinaryThree/BinaryThree.cpp
@@ -0,0 +1,14 @@
+template <typename T>
+struct Node {
+    T value;
+    Node leftNode;
+    Node rightNode;
+};
+
+template <typename T>
+class BinaryThree {
+private:
+    Node<T> rootNode;
+
+public:
+};
